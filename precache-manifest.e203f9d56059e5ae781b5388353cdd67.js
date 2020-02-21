c3671627cde9fb629e3c74886ad179842480bdb5
Juan Tancredi <juan.tancredi@southworks.com>
2020-02-21 16:20:26 -0300

    Updates

---
+++ precache-manifest.e203f9d56059e5ae781b5388353cdd67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44207003709fc616971a971651f1362d",
+    "url": "/Coinbets/index.html"
+  },
+  {
+    "revision": "161dcd7ad53db1801e2a",
+    "url": "/Coinbets/static/css/main.b9a435be.chunk.css"
+  },
+  {
+    "revision": "8e2917126c038f1dd8ca",
+    "url": "/Coinbets/static/js/2.166cf776.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Coinbets/static/js/2.166cf776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "161dcd7ad53db1801e2a",
+    "url": "/Coinbets/static/js/main.0b7ccb1f.chunk.js"
+  },
+  {
+    "revision": "e28290861633abd7f8e1",
+    "url": "/Coinbets/static/js/runtime-main.038de1e9.js"
+  }
+]);
